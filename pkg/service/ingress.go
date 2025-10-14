4e89491ffa2a7a72e14131d26f2a944c00c8881a
vsitnev <vsitnev@sitronics.com>
2025-10-14 12:13:08 +0300

    feat: draft

--- pkg/service/ingress.go
+++ pkg/service/ingress.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/livekit/livekit-server/pkg/config"
 	"github.com/livekit/livekit-server/pkg/telemetry"
+	"github.com/livekit/protocol/ingress"
 	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/protocol/logger"
 	"github.com/livekit/protocol/rpc"
@@ -55,7 +57,18 @@ func (s *IngressService) CreateIngress(ctx context.Context, req *livekit.CreateI
 		AppendLogFields(ctx, fields...)
 	}()
 
-	ig, err := s.CreateIngressWithUrlPrefix(ctx, s.conf.RTMPBaseURL, req)
+	var url string
+	switch req.InputType {
+	case livekit.IngressInput_RTMP_INPUT:
+		url = s.conf.RTMPBaseURL
+	case livekit.IngressInput_WHIP_INPUT:
+		url = s.conf.WHIPBaseURL
+	default:
+		return nil, fmt.Errorf("invalid ingress type: %s", req.InputType)//ingress.ErrInvalidIngressType
+	}
+
+
+	ig, err := s.CreateIngressWithUrlPrefix(ctx, url, req)
 	if err != nil {
 		return nil, err
 	}
@@ -86,9 +99,21 @@ func (s *IngressService) CreateIngressWithUrlPrefix(ctx context.Context, urlPref
 		RoomName:            req.RoomName,
 		ParticipantIdentity: req.ParticipantIdentity,
 		ParticipantName:     req.ParticipantName,
-		Reusable:            req.InputType == livekit.IngressInput_RTMP_INPUT,
+		//Reusable:            req.InputType == livekit.IngressInput_RTMP_INPUT,
 		State:               &livekit.IngressState{},
 	}
+
+	switch req.InputType {
+	case livekit.IngressInput_RTMP_INPUT,
+		livekit.IngressInput_WHIP_INPUT:
+		info.Reusable = true
+		if err := ingress.ValidateForSerialization(info); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, ingress.ErrInvalidIngressType
+	}
+
 
 	if err = s.store.StoreIngress(ctx, info); err != nil {
 		logger.Errorw("could not write ingress info", err)
